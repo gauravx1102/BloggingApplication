57df0872ba898b866b9a2766922b1a2b26400924
fulara1102 <fularagaurav1102@gmail.com>
2024-02-25 23:09:22 +0530

    connecting db

---
+++ blog-app-apis/src/test/java/com/fulara/blogApp/BlogAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fulara.blogApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
